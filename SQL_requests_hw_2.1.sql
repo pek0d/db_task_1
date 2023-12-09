4a03d8b379c8a38d015e57d066bc2d32412c9305
pek0d <96959960+pek0d@users.noreply.github.com>
2023-12-09 19:37:09 +0300

    Update SQL_requests_hw_2.1.sql
    table fill 1

--- SQL_requests_hw_2.1.sql
+++ SQL_requests_hw_2.1.sql
@@ -1,1 +1,27 @@
--- Домашнее задание к лекции «Продвинутая выборка данных»
+-- Добавление в БД таблицы исполнители
+insert into artists (id, name)
+values (1,'Linkin Park');
+insert into artists (id, name)
+values (2,'P.O.D');
+insert into artists (id, name)
+values (3,'Papa Roach');
+insert into artists (id, name)
+values (4,'Tiesto');
+insert into artists (id, name)
+values (5,'Скриптонит');
+
+-- Добавление в БД таблицы жанры
+insert into genres (id, name)
+values (1, 'Rock');
+insert into genres (id, name)
+values (2, 'Pop');
+insert into genres (id, name)
+values (3, 'Rap');
+
+-- Добавление в БД таблицы альбомы
+insert into albums (id, name, release_year)
+values (1, 'Reanimation', 2002);
+insert into albums (id,name, release_year)
+values (2, 'Hybrid Theory', 2000);
+insert into albums (id, name, release_year)
+values (3, 'Lovehatetragedy', 2002);
