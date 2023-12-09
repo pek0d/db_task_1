9f88ae17bd1d0af350897cb3d63a051750bf9075
pek0d <96959960+pek0d@users.noreply.github.com>
2023-12-09 20:32:05 +0300

    Update SQL_requests_hw_2.1.sql

--- SQL_requests_hw_2.1.sql
+++ SQL_requests_hw_2.1.sql
@@ -1,39 +1,39 @@
 -- Добавление в БД таблицы исполнители
-insert into artists (id, name)
-values (1,'Linkin Park');
-insert into artists (id, name)
-values (2,'P.O.D');
-insert into artists (id, name)
-values (3,'Papa Roach');
-insert into artists (id, name)
-values (4,'Tiesto');
-insert into artists (id, name)
-values (5,'Скриптонит');
+INSERT INTO artists (id, name)
+VALUES (1,'Linkin Park');
+INSERT INTO artists (id, name)
+VALUES (2,'P.O.D');
+INSERT INTO artists (id, name)
+VALUES (3,'Papa Roach');
+INSERT INTO artists (id, name)
+VALUES (4,'Tiesto');
+INSERT INTO artists (id, name)
+VALUES (5,'Скриптонит');
 
 -- Добавление в БД таблицы жанры
-insert into genres (id, name)
-values (1, 'Rock');
-insert into genres (id, name)
-values (2, 'Pop');
-insert into genres (id, name)
-values (3, 'Rap');
+INSERT INTO genres (id, name)
+VALUES (1, 'Rock');
+INSERT INTO genres (id, name)
+VALUES (2, 'Pop');
+INSERT INTO genres (id, name)
+VALUES (3, 'Rap');
 
 -- Добавление в БД таблицы альбомы
-insert into albums (id, name, release_year)
-values (1, 'Reanimation', 2002);
-insert into albums (id,name, release_year)
-values (2, 'Hybrid Theory', 2000);
-insert into albums (id, name, release_year)
-values (3, 'Lovehatetragedy', 2002);
+INSERT INTO albums (id, name, release_year)
+VALUES (1, 'Reanimation', 2002);
+INSERT INTO albums (id,name, release_year)
+VALUES (2, 'Hybrid Theory', 2000);
+INSERT INTO albums (id, name, release_year)
+VALUES (3, 'Lovehatetragedy', 2002);
 
 -- Добавление в БД таблицы tracks
-insert into tracks (id, name, duration)
-values (1,'Crawling', 184);
-insert into tracks (id, name, duration)
-values (2,'Youth of the nation', 172);
-insert into tracks (id, name, duration)
-values (3,'She loves me not', 192);
-insert into tracks (id, name, duration)
-values (4,'Just be', 234);
-insert into tracks (id, name, duration)
-values (5,'baby mama', 187);
+INSERT INTO tracks (id, name, duration)
+VALUES (1,'Crawling', 184);
+INSERT INTO tracks (id, name, duration)
+VALUES (2,'Youth of the nation', 172);
+INSERT INTO tracks (id, name, duration)
+VALUES (3,'She loves me not', 192);
+INSERT INTO tracks (id, name, duration)
+VALUES (4,'Just be', 234);
+INSERT INTO tracks (id, name, duration)
+VALUES (5,'baby mama', 187);
