bfa1510e73b723054abaa390a14b60f6350610dd
pek0d <96959960+pek0d@users.noreply.github.com>
2023-12-09 20:06:51 +0300

    Update SQL_requests_hw_2.1.sql

--- SQL_requests_hw_2.1.sql
+++ SQL_requests_hw_2.1.sql
@@ -25,3 +25,15 @@ insert into albums (id,name, release_year)
 values (2, 'Hybrid Theory', 2000);
 insert into albums (id, name, release_year)
 values (3, 'Lovehatetragedy', 2002);
+
+-- Добавление в БД таблицы tracks
+insert into tracks (id, name, duration)
+values (1,'Crawling', 184);
+insert into tracks (id, name, duration)
+values (2,'Youth of the nation', 172);
+insert into tracks (id, name, duration)
+values (3,'She loves me not', 192);
+insert into tracks (id, name, duration)
+values (4,'Just be', 234);
+insert into tracks (id, name, duration)
+values (5,'baby mama', 187);
