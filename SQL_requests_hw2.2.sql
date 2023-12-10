ccd4afc747b81cca331b3a174fd902c75cda193f
pek0d <96959960+pek0d@users.noreply.github.com>
2023-12-10 12:53:37 +0300

    Update SQL_requests_hw2.2.sql

--- SQL_requests_hw2.2.sql
+++ SQL_requests_hw2.2.sql
@@ -3,8 +3,24 @@
 -- Внимание: результаты запросов не должны быть пустыми, учтите это при заполнении таблиц.
 
 -- Название и продолжительность самого длительного трека.
+SELECT name, duration
+FROM tracks
+WHERE duration = (SELECT MAX(duration) FROM tracks);
+
 -- Название треков, продолжительность которых не менее 3,5 минут.
+SELECT name, duration
+FROM tracks
+WHERE duration >= 210;
+
 -- Названия сборников, вышедших в период с 2018 по 2020 год включительно.
+SELECT name, release_year
+FROM compilations
+WHERE release_year BETWEEN 2018 AND 2020;
+
 -- Исполнители, чьё имя состоит из одного слова.
+SELECT name
+FROM artists
+WHERE name ~ '^[A-Za-z-]+$';
+
 -- Название треков, которые содержат слово «мой» или «my».
 
